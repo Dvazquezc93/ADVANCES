10f8ba989eb68f4d7e3a9855dfb6412b713cfacb
Dvazquezchi <danivxinxill@gmail.com>
2026-04-13 19:03:12 +0200

    13/04/2026

--- JAVA/ejercicios-tema-06/src/ejercicios/Ejercicio1/App.java
+++ JAVA/ejercicios-tema-06/src/ejercicios/Ejercicio1/App.java
@@ -1,26 +1,38 @@
 package ejercicios.Ejercicio1;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
 		PersonaService s = new PersonaService();
-		String condicion = "";
+		Boolean existe = true;
 		try (Scanner sc = new Scanner(System.in)) {
 			do {
 				try {
+					existe = true;
 					System.out.println("***BASE DE DATOS PERSONAS*** \nDime un dni:");
 					System.out.println(s.consultarPersonal(sc.nextLine()));
 
 				} catch (PersonaNotFoundException e) {
-					System.out.println("Con ese dni no hay personas en la base de datos");
+					existe = false;
+					System.out.println(e.getMessage());
 				} catch (PersonaException e) {
+					System.out.println(e.getMessage());
 					e.printStackTrace();
 				}
-				System.out.println("¿Quieres comprobar otro dni?");
-				condicion = sc.nextLine();
-			} while (!(condicion.equalsIgnoreCase("no")));
+
+			} while (!(existe));
+		}
+		try {
+			List<Persona>lista =s.BuscarPersonas("l");
+			for (Persona persona : lista) {
+				System.out.println(persona);
+			}
+		} catch (PersonaNotFoundException | PersonaException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 
--- JAVA/ejercicios-tema-06/src/ejercicios/Ejercicio1/PersonaService.java
+++ JAVA/ejercicios-tema-06/src/ejercicios/Ejercicio1/PersonaService.java
@@ -1,12 +1,15 @@
+
 package ejercicios.Ejercicio1;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PersonaService {
 	private Connection openConnection() throws SQLException {
@@ -46,6 +49,60 @@ public class PersonaService {
 			} else {
 				throw new PersonaNotFoundException("No existe persona con este dni " + dni);
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new PersonaException("Error al consultar persona en BBDD", e);
+		}
+
+	}
+
+	public List<Persona> buscarPersonas(String filtro) throws PersonaNotFoundException, PersonaException {
+		try (Connection con = openConnection()) {
+			// 2. Escribir SQL
+			String sql = "select * from personas where nombre like '%'||?||'%' or apellidos like '%'||?||'%'";
+			// 3. Crear PreparedStatement
+			PreparedStatement statement = con.prepareStatement(sql);
+			// 4. Dar Valores a los parámetros
+			statement.setString(1, filtro);
+			statement.setString(2, filtro);
+			List<Persona> lista = new ArrayList<>();
+		
+			// 5. Ejecutar
+			ResultSet rs = statement.executeQuery();
+			while (rs.next()) {
+				Persona p = new Persona();
+				p.setDni(rs.getString("dni"));
+				p.setNombre(rs.getString("nombre"));
+				p.setApellidos(rs.getString("apellidos"));
+				p.setFechaNacimiento(rs.getDate("Fecha_Nacimiento").toLocalDate());
+				lista.add(p);
+			}
+			if (!lista.isEmpty()) {
+				return lista;
+			} else {
+				throw new PersonaNotFoundException("No existe persona que contenga en su nombre " + filtro);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new PersonaException("Error al consultar persona en BBDD", e);
+		}
+
+	}
+	public void insertarPersonas(String dni, String nombre, String apellidos, LocalDate fechaNacimiento) throws  PersonaException {
+		System.out.println("Insertando persona...");
+		try (Connection con = openConnection()) {
+			// 2. Escribir SQL
+			String sql = "insert into personas values(?,?,?,?)";
+			// 3. Crear PreparedStatement
+			PreparedStatement statement = con.prepareStatement(sql);
+			// 4. Dar Valores a los parámetros
+			statement.setString(1, dni);
+			statement.setString(2, nombre);
+			statement.setString(3,apellidos);
+			statement.setDate(4, Date.valueOf(fechaNacimiento));
+	
+			 statement.execute();
+			 System.out.println("Persona insertada...");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new PersonaException("Error al consultar persona en BBDD", e);
