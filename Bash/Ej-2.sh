77c93b1b60324e99e84e138e99739f3d60c53696
Dvazquezchi <danivxinxill@gmail.com>
2026-04-27 17:46:43 +0200

    Create Ej-2.sh

---
+++ Bash/Ej-2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+suma(){
+sum=$(($1+$2))
+return  $sum
+}
+read -p "Ingresa un número: " NUM1
+read -p "Ingresa un segundo número: " NUM2
+suma "$NUM1" "$NUM2"
+echo "El resultado es : suma() " 
